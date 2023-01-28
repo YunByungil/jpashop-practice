18862e3e8e3b05bf23afef61a1a3f109f1b791fe
YunByungil <dbsquddlfz@naver.com>
2023-01-28 14:41:03 +0900

    도메인 분석 설계 완료

---
+++ practice/src/main/java/jpapractice/practice/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package jpapractice.practice.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ practice/src/main/java/jpapractice/practice/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpapractice.practice.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
