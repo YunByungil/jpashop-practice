93a715046924d960c85cb9c460f85cd32a01e47a
YunByungil <dbsquddlfz@naver.com>
2023-01-31 11:24:07 +0900

    주문 검색 기능 완료, 홈 화면 완료

--- practice/src/main/java/jpapractice/practice/repository/OrderRepository.java
+++ practice/src/main/java/jpapractice/practice/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package jpapractice.practice.repository;
 
 import jpapractice.practice.domain.Order;
+import jpapractice.practice.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -21,6 +24,47 @@ public class OrderRepository {
         return em.find(Order.class, orderId);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jqpl = "select o from Order o join o.member m";
+        boolean isFirstCondtion = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondtion) {
+                jqpl += " where";
+                isFirstCondtion = false;
+            } else {
+                jqpl += " and";
+            }
+            jqpl += " o.orderStatus = :orderStatus";
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondtion) {
+                jqpl += " where";
+                isFirstCondtion = false;
+            } else {
+                jqpl += " and";
+            }
+            jqpl += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jqpl, Order.class)
+                .setMaxResults(1000); // 최대 1000건
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("orderStatus", orderSearch.getOrderStatus());
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+
+//        return em.createQuery("select o from Order o join o.member m" +
+//                " where o.orderStatus = :orderStatus" +
+//                " and m.name like :name", Order.class)
+//                .setParameter("orderStatus", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .getResultList();
+    }
 
 }
--- practice/src/main/java/jpapractice/practice/repository/OrderSearch.java
+++ practice/src/main/java/jpapractice/practice/repository/OrderSearch.java
@@ -1,4 +1,12 @@
 package jpapractice.practice.repository;
 
+import jpapractice.practice.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
 }
